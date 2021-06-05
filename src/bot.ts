391be4b86791cddc7c61ad19aa4d0880f5e6c1b1
Dean Shub <deanshub@gmail.com>
2021-06-05 13:56:30 +0300

    search and download

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,19 @@
 import { Telegraf, Context } from 'telegraf'
-import { createTextChangeRange } from 'typescript'
 import WebTorrent from 'webtorrent'
+import TorrentSearchApi from 'torrent-search-api';
+import {set, get } from './cbData'
 
 var client = new WebTorrent()
+
+const providers = TorrentSearchApi.getProviders();
+TorrentSearchApi.enableProvider('1337x')
+// providers.forEach(provider=>{
+//     // @ts-ignore
+//     if(provi''der.public){
+//         console.log(provider)
+//         // TorrentSearchApi.enableProvider(provider.name);
+//     }
+// })
 
 export async function setupBot(): Promise<Telegraf<Context>>{
     if (!process.env.BOT_TOKEN){
@@ -13,17 +24,58 @@ export async function setupBot(): Promise<Telegraf<Context>>{
     bot.help((ctx) => ctx.reply('Send me a sticker'))
     // bot.on('sticker', (ctx) => ctx.reply('👍'))
     // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-    bot.command('download', ctx =>{
+    bot.command('download', async (ctx) =>{
         const magnetURI = getCommandText('download', ctx.message.text)
-
-        client.add(magnetURI, {path: process.env.DOWNLOAD_DIR??process.cwd()} ,(torrent) =>{
-            // Got torrent metadata!
-            ctx.reply(`Downloading ${torrent.name}`)
+        const torrent = await download(magnetURI)
+        torrent.on('done', ()=>{
+            ctx.reply(`${torrent.name} Downloaded`)
+        })
+        torrent.on('error',(e)=>{
+            ctx.reply(`${torrent.name} Failed to download\n${e.toString()}`)
+        })
+        ctx.reply(`Downloading ${torrent.name}`)
+    })
+    bot.command('search', async (ctx)=>{
+        const searchTerm = getCommandText('search', ctx.message.text)
+        const torrents = await TorrentSearchApi.search(searchTerm, 'All', 20).catch(e=>{
+            console.warn(e)
+            return []
+        });
+        torrents.forEach(async torrent=>{
+            const magnet = await TorrentSearchApi.getMagnet(torrent);
+            const key = set(magnet)
+            ctx.replyWithMarkdown(`*${torrent.size}*\n${torrent.title}`, {
+                reply_markup:{
+                    inline_keyboard:[[{
+                        text: '📥 Download',
+                        callback_data: key,
+                    }]],
+                    remove_keyboard:true,
+                    resize_keyboard: true,
+                },
+            })
+        })
+        if (torrents.length===0){
+            ctx.reply('No torrents found')
+        }
+    })
+    bot.on('callback_query', async (ctx)=>{
+        // @ts-ignore
+        const magnet = get(ctx.callbackQuery.data)
+        if (magnet){
+            const torrent = await download(magnet)
             torrent.on('done', ()=>{
                 ctx.reply(`${torrent.name} Downloaded`)
             })
-        })
+            torrent.on('error',(e)=>{
+                ctx.reply(`${torrent.name} Failed to download\n${e.toString()}`)
+            })
+            ctx.reply(`Downloading ${torrent.name}`)
+        }else{
+            ctx.reply(`Can't download, please try later`)
+        }
     })
+
     bot.launch()
     
     // Enable graceful stop
@@ -35,4 +87,12 @@ export async function setupBot(): Promise<Telegraf<Context>>{
 
 function getCommandText(command: string, text:string):string {
     return text.replace(`/${command} `,'')
+}
+
+async function download(magnetURI: string):Promise<WebTorrent.Torrent>{
+    return new Promise((resolve,reject)=>{
+        client.add(magnetURI, {path: process.env.DOWNLOAD_DIR??process.cwd()} ,(torrent) =>{
+            resolve(torrent)
+        })
+    })
 }
---
+++ src/cbData.ts
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const cache = new Map<string,string>()
+
+export function set(data: string): string{
+    const key = uuidv4()
+    cache.set(key, data)
+    return key
+}
+
+export function get(key: string): string|null{
+    if(cache.has(key)){
+        return cache.get(key) ?? null
+    }
+    return null
+}
+
+function clearCache(){
+    for (const [key] of cache) {
+        if (cache.size>50){
+            cache.delete(key)
+        }
+    }
+}
+
+setInterval(clearCache,60*1000)
