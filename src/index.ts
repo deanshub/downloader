17ba99542838fe3db956d9a0cd27f8a9ae77d214
Dean Shub <deanshub@gmail.com>
2021-09-07 18:53:09 +0300

    Initial updater

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { setupBot } from './bot/bot'
 import { loadFromTorrentsDir } from './downloads'
+import { setupAutoUpdate } from './updater'
 
 async function init(): Promise<void> {
     await loadFromTorrentsDir()
     const bot = await setupBot()
+    setupAutoUpdate(bot)
 }
 
 process.on('uncaughtException', console.error)
---
+++ src/updater.ts
@@ -0,0 +1,96 @@
+import { Telegraf, Context } from 'telegraf'
+import { ToadScheduler, SimpleIntervalJob, AsyncTask } from 'toad-scheduler'
+import axios from 'axios'
+import { getAdmin } from './isAdmin'
+
+export async function setupAutoUpdate(bot: Telegraf<Context>) {
+    const scheduler = new ToadScheduler()
+    const task = new AsyncTask(
+        'check for new release', 
+        () => { return checkForUpdate(bot) },
+        (err: Error) => { /* handle error here */ }
+    )
+    
+    if (process.env.UPDATE_INTERVAL !== '0') {
+        const intervalString = process.env.UPDATE_INTERVAL || '1d'
+
+        const interval = convertToScheduleerInterval(intervalString)
+
+        const job = new SimpleIntervalJob({ ...interval, runImmediately: true}, task)
+        
+        scheduler.addSimpleIntervalJob(job)
+    }
+}
+
+function convertToScheduleerInterval(intervalString: string) {
+    const lastLetter = intervalString[intervalString.length-1]
+    const timeFrame = convertLetterToTimeFrame(lastLetter)
+    const intervalAmount = Number(intervalString.substring(0, intervalString.length-2))
+    if (isNaN(intervalAmount)){
+        throw new Error(`Unkown inteval "${intervalAmount}" please use a known pattern like "1d"`)
+    }
+    return {[timeFrame]: intervalAmount}
+}
+
+function convertLetterToTimeFrame(letter: string): 'seconds' |'minutes'|'hours'|'days' {
+    switch (letter) {
+        case 's':
+            return 'seconds'
+            break
+        case 'm':
+            return 'minutes'
+            break
+        case 'h':
+            return 'hours'
+            break
+        case 'd':
+            return 'days'
+            break
+        default:
+            console.warn(`Uknown interval letter "${letter}" please use d\\h\\m\\s`)
+            return 'days'
+    }
+}
+
+type Releases = Release[]
+interface Release {
+    body: string | undefined
+    published_at: string | undefined
+    created_at: string | undefined
+    tag_name: string | undefined
+    name: string | undefined
+}
+
+async function checkForUpdate(bot: Telegraf<Context>) {
+    // TODO: Maybe get the url from origin remote and fallback to deanshub/downloader?
+    const latestReleaseUrl = `https://api.github.com/repos/deanshub/downloader/releases?per_page=1`
+    const response = await axios.get<Releases>(latestReleaseUrl, {
+        headers: {
+          'Accept': 'application/vnd.github.v3+json'
+        }
+      })
+    const latestRelease = response.data?.[0]
+
+    const latstCommitDate = getLatestCommitDate()
+    const releaseDate = latestRelease?.published_at ?? latestRelease?.created_at
+    if (releaseDate?.getTime() > latstCommitDate?.getTime()) {
+        const releaseVersion = latestRelease?.tag_name ?? latestRelease?.name
+
+        bot.telegram.sendMessage(
+            getAdmin(),
+            `<b>A new version is available ${releaseVersion}</b>\nReleased on ${releaseDate.toLocaleString('he')}
+Press /pull to update\nRelease notes:\n<pre>${
+    latestRelease?.body??'No release notes'
+}</pre>`,
+            {parse_mode: 'HTML'},
+        )
+    }
+}
+
+function getLatestCommitDate(): Date{
+    'git log -1 --format=%cd'
+    const { stdout } = await execa('git', ['log', '-1', '--format=%cd'], {
+        cwd: process.cwd(),
+    })
+    return new Date(stdout)
+}
