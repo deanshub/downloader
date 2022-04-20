76b57c1c418df3c0b676c6c155907ff05c1fbc32
Dean Shub <deanshub@gmail.com>
2022-04-20 15:32:35 +0300

    storage refactor

--- src/getStorageDetails.ts
+++ src/getStorageDetails.ts
@@ -7,6 +7,7 @@ const downloadsDir = process.env.DOWNLOAD_DIR ?? process.cwd()
 interface StorageDetails {
     diskPath: string
     freePercentage: number
+    freeSpace: string
     takenSpace: string
     totalSpace: string
 }
@@ -16,6 +17,7 @@ export async function getStorageDetails(): Promise<StorageDetails> {
     return {
         diskPath: diskSpace.diskPath,
         freePercentage: Math.round(diskSpace.free/diskSpace.size *100),
+        freeSpace: prettysize(diskSpace.free),
         takenSpace: prettysize(diskSpace.size - diskSpace.free),
         totalSpace: prettysize(diskSpace.size),
     }
