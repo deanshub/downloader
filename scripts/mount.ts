cb5e135346fe3fb24c2848e807967e9db23799a3
Dean Shub <deanshub@gmail.com>
2023-08-24 09:40:35 +0300

    fix mount

--- scripts/mount.ts
+++ scripts/mount.ts
@@ -4,8 +4,8 @@ import fs from 'fs-extra'
 async function mount() {
     const { stdout } = await execa('fdisk', ['-l'], {
         cwd: process.cwd(),
-        stdio: 'inherit',
     })
+    
     if (!stdout.includes('/dev/sdb1')) {
         throw new Error('No external drive found')
     }
