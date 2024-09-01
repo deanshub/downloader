f5dd132032df6e5a25495c143a249de209afbdd9
Dean Shub <deanshub@gmail.com>
2024-09-01 18:17:28 +0300

    no update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { update } from './update/update'
+// import { update } from './update/update'
 import { setupBot } from './bot/bot'
 import { loadFromTorrentsDir } from './torrents'
-import { setupAutoUpdate } from './updater'
+// import { setupAutoUpdate } from './updater'
 
 async function init(): Promise<void> {
-    await update()
+    // await update()
     const bot = await setupBot()
     await loadFromTorrentsDir(bot)
-    setupAutoUpdate(bot).catch(console.warn)
+    // setupAutoUpdate(bot).catch(console.warn)
 }
 
 // process.on('uncaughtException', console.error)
