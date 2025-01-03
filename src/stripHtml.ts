1b39f147480015687d44ffb95fdc9a1d5d5c95d8
Dean Shub <deanshub@gmail.com>
2025-01-03 22:06:30 +0200

    wip

--- src/stripHtml.ts
+++ src/stripHtml.ts
@@ -1,3 +1,8 @@
-export function stripHtml(html: string|undefined): string {
-    return (html??'').replace(/\</g, '&lt;').replace(/\>/g, '&gt;')
+export function stripHtml(html: string | undefined): string {
+    return (html ?? '')
+        .replace(/\</g, '&lt;')
+        .replace(/\>/g, '&gt;')
+        .replace(/\&/g, '&amp;')
+        .replace(/\"/g, '&quot;')
+        .replace(/\'/g, '&#039;')
 }
